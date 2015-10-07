72c5e4e8a2605b5cddf188c5ac233e1ddac72181
Paul Semel <paul.semel@epita.fr>
2015-10-07 21:07:51 +0200

    lel

--- paul/objectMarker.c
+++ paul/objectMarker.c
@@ -10,10 +10,56 @@ int main(int argc, char *argv[])
 {
   if(checkoptions(argc, argv) == 0) return 0;
   if(testoptions() == 0) return 0;
+  init_sdl();
+  
   //char *lol = "images/myimage.jpg";
   //savedMarkedImage(lol, 0, 0, 0, 0);
   return 0;
 }
+
+void init_sdl(void) {
+  // Init only the video part
+  if( SDL_Init(SDL_INIT_VIDEO)==-1 ) {
+    // If it fails, die with an error message
+    errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+  }
+  // We don't really need a function for that ...
+}
+
+SDL_Surface* load_image(char *path) {
+  SDL_Surface          *img;
+  // Load an image using SDL_image with format detection
+  img = IMG_Load(path);
+  if (!img)
+    // If it fails, die with an error message
+    errx(3, "can't load %s: %s", path, IMG_GetError());
+  return img;
+}
+
+SDL_Surface* display_image(SDL_Surface *img) {
+  SDL_Surface          *screen;
+  // Set the window to the same size as the image
+  screen = SDL_SetVideoMode(img->w, img->h, 0, SDL_SWSURFACE|SDL_ANYFORMAT);
+  if ( screen == NULL ) {
+    // error management
+    errx(1, "Couldn't set %dx%d video mode: %s\n",
+         img->w, img->h, SDL_GetError());
+  }
+ 
+  /* Blit onto the screen surface */
+  if(SDL_BlitSurface(img, NULL, screen, NULL) < 0)
+    warnx("BlitSurface error: %s\n", SDL_GetError());
+ 
+  // Update the screen
+  SDL_UpdateRect(screen, 0, 0, img->w, img->h);
+ 
+  // wait for a key
+  wait_for_keypressed();
+ 
+  // return the screen for further uses
+  return screen;
+}
+
 
 void savedMarkedImage(char *imagepath, int width, int height, int marginleft, int margintop)
 {
