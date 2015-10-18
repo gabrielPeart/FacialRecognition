7f4874237ed0e55d5df8e2185dfd4664f569d091
Paul Semel <paul.semel@epita.fr>
2015-10-18 15:06:05 +0200

    Integral image

--- Viola-Jones/integral_image.c
+++ Viola-Jones/integral_image.c
@@ -1,19 +1,21 @@
 #include "integral_image.h"
 #include "load_image.h"
+#include "pixel_operations.h"
 
 void integral_image(t_image *image)
 {
   int x, y;
-
+  
   init_integral_image(image);
-  for (int i = 0; i < image->surface->w; ++i)
-    image->integral[0][i] = i + 1;
-  for (int i = 0; i < image->surface->h; ++i)
-    image->integral[i][0] = i + 1;
+  image->integral[0][0] = (int)getpixel(image->surface, 0, 0)
+  for (int i = 1; i < image->surface->w; ++i)
+    image->integral[0][i] = image->integral[0][i-1] + (int)getpixel(image->surface, (unsigned)i, 0);
+  for (int i = 1; i < image->surface->h; ++i)
+    image->integral[i][0] = image->integral[i-1][0] + (int)getpixel(image->surface, 0, (unsigned)i;
 
   for (y = 1; y < image->surface->h; ++y)
     for (x = 1; x < image->surface->w; ++x)
-      image->integral[y][x] = 1 + image->integral[y - 1][x]
+      image->integral[y][x] = (int)getpixel(image->surface, (unsigned)x, (unsigned)y) + image->integral[y - 1][x]
                                 + image->integral[y][x - 1]
                                 - image->integral[y - 1][x - 1];
 }
