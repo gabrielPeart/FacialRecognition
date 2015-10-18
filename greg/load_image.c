fb875d96e8e5c882a6206627aa0b806ef5200037
Grégoire Verdier <gregoire.verdier@epita.fr>
2015-10-18 12:23:16 +0200

    Integral image & shit

--- greg/load_image.c
+++ greg/load_image.c
@@ -1,8 +1,10 @@
 #include "load_image.h"
+#include "pixel_operations.h"
 #include <stdlib.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
 #include <err.h>
+#include <assert.h>
 
 void display_image(SDL_Surface *image)
 {
@@ -12,7 +14,6 @@ void display_image(SDL_Surface *image)
   position.x = 0;
   position.y = 0;
 
-  SDL_Init(SDL_INIT_VIDEO);
   screen = SDL_SetVideoMode(image->w, image->h, 32, SDL_HWSURFACE);
   SDL_BlitSurface(image, NULL, screen, &position);
   SDL_Flip(screen);
@@ -30,19 +31,49 @@ t_image *load_image(const char *img_name)
     warn("%s ", img_name);
     return NULL;
   }
-  //display_image(image->surface);
+  image->bw = convert_to_grey_level(image->surface);
+  init_sdl();
+  display_image(image->surface);
+  display_image(image->bw);
   return image;
 }
 
-void sleep()
+void init_sdl() {
+  if( SDL_Init(SDL_INIT_VIDEO)==-1 )
+    errx(1,"Could not initialize SDL: %s.\n", SDL_GetError());
+}
+
+SDL_Surface *convert_to_grey_level(SDL_Surface *surface)
 {
-  char b = 1;
+  Uint32 pixel;
+  Uint8 r, g, b, average;
+  unsigned h, w;
+  SDL_Surface *bw;
+
+  bw = SDL_ConvertSurface(surface, surface->format, surface->flags);
+  h = (unsigned)bw->h;
+  w = (unsigned)bw->w;
+  for (unsigned y = 0; y < h; ++y)
+  {
+    for (unsigned x = 0; x < w; ++x)
+    {
+      pixel = getpixel(bw, x, y);
+      SDL_GetRGB(pixel, bw->format, &r, &g, &b);
+      average = r * 0.3 + g * 0.59 + b * 0.11;
+      putpixel(bw, x, y, SDL_MapRGB(bw->format, average, average, average));
+    }
+  }
+  return bw;
+}
+
+void sleep() {
   SDL_Event event;
 
-  while (b)
-  {
-    SDL_WaitEvent(&event);
-    if (event.type == SDL_QUIT)
-      b = 0;
+  for (;;) {
+    SDL_PollEvent( &event );
+    switch (event.type) {
+      case SDL_KEYDOWN: return;
+      default: break;
+    }
   }
 }
--- greg/load_image.h
+++ greg/load_image.h
@@ -6,12 +6,15 @@
 typedef struct s_image t_image;
 
 void            display_image(SDL_Surface *image);
-t_image         *load_image(const char *img_name);
+t_image        *load_image(const char *img_name);
+void            init_sdl();
+SDL_Surface    *convert_to_grey_level(SDL_Surface *surface);
 void            sleep();
 
 struct          s_image
 {
-  SDL_Surface   *surface;
+  SDL_Surface    *surface;
+  SDL_Surface    *bw;
   unsigned long **integral;
 };
 
